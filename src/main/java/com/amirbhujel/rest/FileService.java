f2df9a14bc7919b6c18af4d59e368b8f72f98f46
gandakian <bhujel.amir@gmail.com>
2017-05-31 01:52:48 -0500

    Completed implementing the Upload and Download attachments

--- src/main/java/com/amirbhujel/rest/FileService.java
+++ src/main/java/com/amirbhujel/rest/FileService.java
@@ -6,11 +6,46 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 
 public class FileService {
 
-	private static final String FILE_PATH = "/Users/bharaththippireddy/Desktop/uploads/uploaded.jpg";
+	private static final String FILE_PATH = "/Users/amirbhujel/Documents/SpringSTS/EclipseWorkspace/restattachments/uploads/uploaded.jpg";
 
+	@POST
+	@Path("/upload")
+	public void upload(List<Attachment> attachments){
+		
+		for(Attachment attachment : attachments){
+			try {
+				InputStream inputStream = attachment.getDataHandler().getInputStream();
+				copyFile(inputStream);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@GET
+	@Path("/download")
+	public Response download(){
+		
+		File file = new File(FILE_PATH);
+		ResponseBuilder responseBuilder = Response.ok(file);
+		responseBuilder.header("Content-Disposition", "attachment;filename=downloaded.jpg");
+		
+		return responseBuilder.build();
+	}	
+	
+	
 	private void copyFile(InputStream inputStream) throws FileNotFoundException, IOException {
 		OutputStream out = null;
 		int read = 0;
